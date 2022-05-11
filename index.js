7f7d66799f279e000a456f0a1ebdd197e0a33f65
sdshuvo98 <hellosdshuvo@gmail.com>
2022-05-11 12:34:35 +0600

    get services

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors())
 app.use(express.json())
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nxi2d.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const serviceCollection = client.db("doctors_portal").collection("services");
+
+        app.get('/service', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services)
+        })
+    }
+    catch (error) {
+        console.error(error);
+    }
+}
+run().catch(console.dir());
 
 app.get('/', (req, res) => {
     res.send('Hello from Doctor Website!')
